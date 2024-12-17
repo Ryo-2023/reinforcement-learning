cdf0aa9b78dac12de3da8775ba62f654c258ddf7
Ryo-2023 <ryo-tennis-2017@docomo.ne.jp>
2024-12-17 15:46:15 +0900

    removed unnecessary files

--- get_device_info.py
+++
@@ -1,7 +0,0 @@
-import os
-import torch
-import multiprocessing as mp
-
-print("cpu_count:", os.cpu_count())
-print("gpu_count:", torch.cuda.device_count())
-print("process_count:", torch.cuda.get_device_properties(torch.cuda.current_device))
--- test.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-t = torch.tensor([1, 2, 3])
-a = t.repeat(3, 2)
-print(a)
