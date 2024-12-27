69621aeb4b4a0312d958f688a722f0b9106137a6
ono.ryosuke.36t@st.kyoto-u.ac.jp <ono.ryosuke.36t@st.kyoto-u.ac.jp>
2024-12-27 19:18:43 +0900

    change graph title

--- plot_transition.py
+++ plot_transition.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 
 # データの準備
-with open("E:/sotsuron/venv_sotsuron/src/data/comfort/data_action.pkl", "rb") as f:
+with open("E:/sotsuron/venv_sotsuron/src/data/mse/data_action.pkl", "rb") as f:
     data = pickle.load(f)
 
 # ヒートマップの作成
@@ -12,9 +12,9 @@ plt.figure(figsize=(10, 8))
 sns.heatmap(data, annot=True, cmap="YlGnBu", cbar=True, xticklabels=["Attention 1", "Attention 2", "Attention 3"], yticklabels=np.arange(1, len(data)+1))
 plt.xlabel("Attention Layers")
 plt.ylabel("Time Steps")
-plt.title("Attention Layer Transitions Over Time")
+plt.title("Action Transition   (reward:mse)")
 
 # ファイルに保存
-plt.savefig("E:/sotsuron/venv_sotsuron/src/data/comfort/action_transition.png")  # reward = mse
+plt.savefig("E:/sotsuron/venv_sotsuron/src/data/mse/action_transition.png")  # reward = mse
 plt.show()
 
