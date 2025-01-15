b562be920a2fd066ac1d0e9699796fb1962ed51d
Ryo-2023 <ryo-tennis-2017@docomo.ne.jp>
2025-01-15 13:29:58 +0900

    add calc_alpha_ab

--- pomdp_gpu.py
+++ pomdp_gpu.py
@@ -132,10 +132,10 @@ class Model():
 class PBVI():
     def __init__(self, model, holizon, discount_factor=0.9):
         self.model = model
-        self.horizon = holizon
-        self.discount_factor = discount_factor
+        self.T = holizon
+        self.gamma = discount_factor
         
-        self.alpha_vecs = [torch.zeros(self.model.num_states) for _ in range(holizon)]  # 初期のαベクトル  
+        self.alpha_vecs = [torch.zeros(self.model.num_states) for _ in range(self.T)]  # 初期のαベクトル  
         self.belief_points = self.generate_belief_points(self.model.num_states) # 信念点のリスト
         self.num_belief_points = len(self.belief_points)
         
@@ -155,13 +155,32 @@ class PBVI():
         
         while True:
             # calc alpha_ao
-            alpha_ao = torch.stack([torch.einsum('sj,jao,j->aos',self.model.trans_prob, self.model.obs_prob, self.alpha_vecs[i]) for i in range(self.horizon)])  # [a1(s1),a2(s2),...,aT(sT)], a_k(s):[a,o,s],  alpha_ao:[a,o,s,T]
+            alpha_ao = torch.stack([torch.einsum('sj,jao,j->aos',self.model.trans_prob, self.model.obs_prob, self.alpha_vecs[i]) for i in range(self.T)])  # alpha_ao=[a1(s1),a2(s2),...,aT(sT)], a_k(s):[a,o,s],  alpha_ao:[a,o,s,T], j:s'
             # calc alpha_ab
             # r^a(s)
             r_a = torch.zeros(self.model.num_actions, self.model.num_states)
             for i in range(self.model.num_states):
                 r_a[:,i] = self.model.reward(self.model.states[i])
             
+            alpha_ab = torch.tensor([])
+            for i in range(self.num_belief_points):
+                # b・α^{a,o}
+                b_dot_alpha_ao = torch.einsum('s,aosT->aoT',self.belief_points[i],alpha_ao)  # b[i]・α^{a,o}=[a1,b1],a_k(s):[a,o,s],  b・α^{a,o}:[a,o,T]'
+                argmax_a_index = torch.argmax(b_dot_alpha_ao, dim=2)  # argmax_a:[a,o]
+
+                # argmax_a の indexに対応する値の行列を作成
+                index = argmax_a_index.unsqueeze(-1).unsqueeze(-1).expand(-1,-1,alpha_ao.size(2),-1)  # torch.gather のために整形
+                argmax_a = torch.gather(alpha_ao,3,index).squeeze(-1)  # argmax_a:[a,o,s]
+                sum_alpha_ao = torch.einsum('aos->as',argmax_a)        # sum_alpha_ao:[a,s]
+                
+                
+            
+            # calc alpha_ab
+            alpha_ab = r_a + self.gamma * sum_o  # alpha_ab:[a,s]
+            
+            # calc new_alpha
+            b_dot_alpha_ab = torch.einsum('sn,as->an',self.belief_points,alpha_ab)  # b・alpha_ab:[a,n]
+            new_alpha = torch.max()
         
 class POMDP(Model):
     def __init__(self, sigma, num_people):
